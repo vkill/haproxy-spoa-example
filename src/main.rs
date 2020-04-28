d543f5c3612ec1529ab666de91f8fe6c4f75b12f
vkill <vkill.net@gmail.com>
2020-04-28 22:10:19 +0800

    replace async-std to smol

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-use async_std::{
-    net::{TcpListener, TcpStream, ToSocketAddrs},
-    prelude::*,
-    task,
-};
 use futures::TryStreamExt;
+use smol::{Async, Task};
+use std::net::{TcpListener, TcpStream};
+
 use futures_codec::Framed;
 use log::*;
 use std::convert::TryInto;
@@ -33,28 +31,27 @@ pub use frames::*;
 fn main() -> anyhow::Result<()> {
     env_logger::init();
 
-    task::block_on(accept_loop("127.0.0.1:6001"))
+    smol::run(accept_loop("127.0.0.1:6001"))
 }
 
-async fn accept_loop(addr: impl ToSocketAddrs) -> anyhow::Result<()> {
-    let listener = TcpListener::bind(addr).await?;
+async fn accept_loop(addr: &str) -> anyhow::Result<()> {
+    let listener = Async::<TcpListener>::bind(addr)?;
 
-    let mut incoming = listener.incoming();
-    while let Some(stream) = incoming.next().await {
-        let stream = stream?;
-        info!("Accepting from: {}", stream.peer_addr()?);
+    loop {
+        let (stream, peer_addr) = listener.accept().await?;
+        println!("Accepted client: {}", peer_addr);
 
-        task::spawn(async move {
+        // Spawn a task that echoes messages from the client back to it.
+        Task::spawn(async move {
             if let Err(e) = connection_loop(stream).await {
                 error!("connection error: {:?}", e)
             }
-        });
+        })
+        .detach();
     }
-
-    Ok(())
 }
 
-async fn connection_loop(stream: TcpStream) -> anyhow::Result<()> {
+async fn connection_loop(stream: Async<TcpStream>) -> anyhow::Result<()> {
     let mut framed = Framed::new(stream, FrameCodec());
     while let Some(mut bytes) = framed.try_next().await? {
         debug!("frame bytes: {:?}", bytes);
